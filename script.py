7b2f64f1003a7148e44d1c968fce75b1068f0f72
Daniel Pullan <dandandrumsgm@gmail.com>
2017-02-04 13:57:04 +0000

    make it poke websites
    and then do stuff with the result

--- script.py
+++ script.py
@@ -1,20 +1,29 @@
 # i had to pip-install simplepush, if you have issues, installing python-dev
 # fixed it for me
 from simplepush import send_encrypted
+# imported requests in order to have something to play with
+import requests
 # i made a config file defining key, password and salt so that I could upload
 # it once and then gitignore it to avoid encryption keys being uploaded
 from config import *
 
-
-# make calling push use simplepush to send a notification to phone
+# make different pushes to send a notification to phone with simplepush
 push = send_encrypted (key, password, salt, "title", "message", "event")
+downAlert = send_encrypted (key, password, salt, "Website", "Server is down")
+upAlert = send_encrypted (key, password, salt, "Website", "Server is up")
+# prod the bee's nest and see what happens
+response = requests.get('http://danielpullan.co.uk')
 
-# make a value to have a number to go True or False against
-number = 4563
+# print the resulting mess
+print(int(response.status_code))
 
-# if the number isn't equal to 1, call our push event
-if number != 1:
-        push
-# else,print "don't push" as a way of knowing the value returned False
+# response 200 means all is well
+if response.status_code == 200:
+    # so announce all is well
+    print "website up"
+    upAlert
+# else it isn't
 else:
-        print ("don't push")
+    #so announce that all is not well
+    print "website down"
+    downAlert
